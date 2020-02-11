1ad79777817585fc2be1beda9ca4ea0664e1510f
Luis Díaz Manzo <luis@drconopoima.com>
2020-02-11 22:46:10 +0100

    Added App POST to delete listing with body parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 import { listings } from "./listings";
 
 const app = express();
 const port: number = 8085;
+
+app.use(bodyParser.json());
 
 app.get("/", (_request, response) =>
   response.send(`Hello visitor of port ${port}!`)
@@ -11,6 +13,19 @@ app.get("/", (_request, response) =>
 
 app.get("/listings", (_request, response) => {
   response.send(listings);
+});
+
+app.post("/listings/delete", (request, response) => {
+  const id: string = request.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      const deletedListing = listings.splice(i, 1);
+      return response.send(deletedListing);
+    }
+  }
+
+  return response.send(`Failed to delete listing of id=${id}.`);
 });
 
 app.listen(port);
