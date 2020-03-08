ce6681c7bb11ef4e4a960f72a36bda1b5b4ae266
Luis Díaz Manzo <luis@drconopoima.com>
2020-03-08 22:32:37 +0100

    Added dotenv. Extract constants & sensitive data to env variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,28 +1,18 @@
 import { MongoClient } from "mongodb";
 import { Database } from "../lib/types";
 
-// Shared Details
-const userMongo = "<user>";
-const dbMongo = "<db_name>";
-const collectionMongo = "<collection_name>";
-const passwordMongo = "<password>";
-
-// MongoDB Atlas
-const clusterMongo = "<atlas-cluster-id>";
-const uri = `mongodb+srv://${userMongo}:${passwordMongo}@${clusterMongo}.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_USER_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
 
 // // MongoDB OnPrem
-// const host = "<ip>";
-// const port = "<port>"
-// const uri = `mongodb://<${host}>:${port}/test`
+// const uri = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/test`
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
-  const db = client.db(dbMongo);
+  const db = client.db(process.env.MONGODB_DATABASE);
   return {
-    listings: db.collection(collectionMongo)
+    listings: db.collection(`${process.env.MONGODB_LISTINGS_COLLECTION}`)
   };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+require("dotenv").config();
+
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
-
-const port: number = 8085;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
@@ -16,7 +16,7 @@ const mount = async (app: Application) => {
   });
   server.applyMiddleware({ app, path: "/api" });
 
-  app.listen(port);
+  app.listen(process.env.PORT);
 
   const listings = await db.listings.find({}).toArray();
   console.log(listings);
